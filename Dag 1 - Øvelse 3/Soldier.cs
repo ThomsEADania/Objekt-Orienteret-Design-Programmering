1e1375320f4d592d6bbd55aee6f1dfb4a6cc7cf7
ThomsEADania <thmo5@student.eadania.dk>
2024-10-21 12:11:52 +0200

    Dag 1 - Øvelse 3
    Klarede øvelse 3 fra dag 1

---
+++ Dag 1 - Øvelse 3/Soldier.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Numerics;
+
+namespace Dag_1___Øvelse_3
+{
+    internal class Soldier
+    {
+        public int health = 100;
+
+        private int armor;
+
+        private int energy;
+
+        private float speed;
+
+        private string name;
+
+        private Weapon weapon;
+
+        public Vector2 position;
+
+        /// <summary>
+        /// Increments soldier's x position
+        /// </summary>
+        public void Move()
+        {
+            position.X++;
+        }
+        /// <summary>
+        /// Writes "I'm jumping!" to the console and increments soldier's y position
+        /// </summary>
+        public void Jump()
+        {
+            Console.WriteLine("I'm jumping!");
+            
+            position.Y++;
+        }
+        /// <summary>
+        /// Writes "BANG!" to the console
+        /// </summary>
+        public void Shoot()
+        {
+            Console.ForegroundColor = ConsoleColor.DarkRed;
+            
+            Console.WriteLine("BANG!");
+
+            Console.ForegroundColor = ConsoleColor.Gray;
+        }
+    }
+}
---
+++ Dag 1 - Øvelse 3/Weapon.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dag_1___Øvelse_3
+{
+    internal class Weapon
+    {
+    }
+}
