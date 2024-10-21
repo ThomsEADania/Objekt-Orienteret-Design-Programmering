f0fe1503d43148f12a4b52f709236925f13ef162
ThomsEADania <thmo5@student.eadania.dk>
2024-10-21 10:16:51 +0200

    Add project files.

---
+++ Dag 1 - Øvelse 1/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dag_1___Øvelse_1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Soldier soldier = new Soldier();
+        }
+    }
+}
---
+++ Dag 1 - Øvelse 1/Soldier.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dag_1___Øvelse_1
+{
+    internal class Soldier
+    {
+    }
+}
