a810e20d355916ce91826c504ac0e582578bd8b9
ThomsEADania <thmo5@student.eadania.dk>
2024-10-22 10:54:03 +0200

    Dag 2 - Øvelse 1 (+tilføjede D1Ø6)
    Klarede øvelse 1 fra dag 2 og tilføjede filer til øvelse 6 fra dag 1

---
+++ Dag 1 - Øvelse 6/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Numerics;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dag_1___Øvelse_5
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Soldier soldier1 = new Soldier(); //Instanciates new Soldier object
+            Soldier soldier2 = new Soldier(150); //Instanciates new Soldier object with 150 health
+
+            bool keepPlaying = true;
+
+            while (keepPlaying)
+            {
+                Console.Clear();
+
+                Console.WriteLine("The soldier's health is: " + soldier1.Health); //Display soldier1's health
+
+                Console.WriteLine(soldier1.position); //Displays soldier1's position (vector2)
+
+                soldier1.Move();
+
+                soldier1.Jump();
+
+                soldier1.Shoot();
+
+                Console.WriteLine("Do you want to continue? (Y/N)");
+
+                string answer = Console.ReadLine().ToLower();
+
+                if (answer == "n")
+                {
+                    keepPlaying = false;
+                }
+            }
+
+            Console.Clear();
+            Console.WriteLine("Press any key to close the program");
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Dag 2 - Øvelse 1/Score.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dag_2___Øvelse_1
+{
+    internal class Score
+    {
+        #region Fields (AKA attributes)
+        //Are not read- or write-able outside of this class. Means HighscoreList cannot read Score values (without adding public property to get score values)
+        private string name;
+        private int points;
+        #endregion
+
+        #region Constructors
+        public Score(string name, int points)
+        {
+            this.name = name;
+            this.points = points;
+        }
+        #endregion
+    }
+}
