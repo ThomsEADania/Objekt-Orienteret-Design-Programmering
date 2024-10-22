170663bfdf54e290259a77b9d8c33584e195e50b
ThomsEADania <thmo5@student.eadania.dk>
2024-10-22 11:49:32 +0200

    Dag 2 - Øvelse 2
    Klarede øvelse 2 fra dag 2

---
+++ Dag 2 - Øvelse 2/HighscoreList.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dag_2___Øvelse_1
+{
+    internal class HighscoreList
+    {
+        #region Fields
+        private static Score[] scores = new Score[10]; //Instanciates new array of type Score
+        #endregion
+
+        #region Properties
+        /// <summary>
+        /// Property for returning scores array (whole array)
+        /// </summary>
+        public static Score[] Scores
+        {
+            get { return scores; }
+        }
+        #endregion
+
+        #region Methods (Functions)
+        /// <summary>
+        /// Places score object in scores array at index 0
+        /// </summary>
+        /// <param name="score">Score object</param>
+        public static void AddScore(Score score)
+        {
+            scores[0] = score;
+        }
+        public static void PrintToScreen()
+        {
+            for (byte i = 0; i < scores.Length; i++)
+            {
+                if (scores[i] != null)
+                {
+                    Console.WriteLine(scores[i].Display());
+                    break;
+                }
+            }
+        }
+        #endregion
+    }
+}
---
+++ Dag 2 - Øvelse 2/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dag_2___Øvelse_1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            HighscoreList.AddScore(new Score("Thoms", 100));
+
+            HighscoreList.PrintToScreen();
+
+
+
+            Console.WriteLine("\nPress any key to close the program");
+            Console.ReadKey();
+        }
+    }
+}
