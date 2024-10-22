884d578dc240fa1299c32e556e87a5b215049c1a
ThomsEADania <thmo5@student.eadania.dk>
2024-10-22 14:50:33 +0200

    Omdøbte øvelse 6 til 6a + øvelse 6b filer
    Omdøbte dag 1 øvelse 6 til øvelse 6a og lavede filer til øvelse 6b

---
+++ Dag 1 - Øvelse 6b/Weapon.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dag_1___Øvelse_6b
+{
+    internal class Weapon
+    {
+        private int ammo;
+
+        public int Ammo
+        {
+            get { return ammo; }
+            set { ammo = value; }
+        }
+    }
+}
