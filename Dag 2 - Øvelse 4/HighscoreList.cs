b7712808b7f044158def6c077159238f59a1063b
ThomsEADania <thmo5@student.eadania.dk>
2024-10-31 14:41:53 +0100

    Dag 2  - Øvelse 4
    Klarede øvelse 4 fra dag 2

---
+++ Dag 2 - Øvelse 4/HighscoreList.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dag_2___Øvelse_4
+{
+    internal class HighscoreList
+    {
+        #region Fields
+        private static Score[] scores = new Score[1]; //Instanciates new array of type Score
+        #endregion
+
+        #region Properties
+        /// <summary>
+        /// Property for returning scores array (whole array)
+        /// </summary>
+        public static Score[] Scores
+        {
+            get { return scores; }
+        }
+        #endregion
+
+        #region Methods (Functions)
+        /// <summary>
+        /// Places score object in scores array at first empty cell, until the end of the array is reached
+        /// </summary>
+        /// <param name="score">Score object</param>
+        public static void AddScore(Score score)
+        {
+            for (byte i = 0; i < scores.Length; i++)
+            {
+                if (scores[i] == null)
+                {
+                    scores[i] = score;
+
+                    Array.Resize(ref scores, scores.Length + 1);
+                    break;
+                }
+            }
+        }
+        /// <summary>
+        /// Displays all values in array if they are not null. Also checks if there is more room in array
+        /// </summary>
+        /// <returns>True if the array is full</returns>
+        public static bool PrintToScreen()
+        {
+            bool arrayFull = false;
+
+            //if (scores[scores.Length - 1] != null)
+            //{
+            //    Console.WriteLine("Cannot store more scores");
+
+            //    arrayFull = true;
+            //}
+
+            for (byte i = 0; i < scores.Length; i++)
+            {
+                if (scores[i] != null)
+                {
+                    Console.WriteLine(scores[i].ToString());
+                }
+            }
+
+            return arrayFull;
+        }
+        #endregion
+    }
+}
---
+++ Dag 2 - Øvelse 4/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dag_2___Øvelse_4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            bool running = true;
+
+            bool showPrompt = true;
+
+            while (running)
+            {
+                Console.Clear();
+
+                showPrompt = !HighscoreList.PrintToScreen();
+
+                string inputName;
+                int inputScore;
+
+                if (showPrompt)
+                {
+                    Console.WriteLine("Input your name:");
+
+                    inputName = Console.ReadLine();
+
+                    Console.WriteLine("Input your score:");
+
+                    inputScore = int.Parse(Console.ReadLine());
+                }
+                else
+                {
+                    break;
+                }
+
+                HighscoreList.AddScore(new Score(inputName, inputScore));
+            }
+
+            Console.WriteLine("\nPress any key to close the program");
+            Console.ReadKey();
+        }
+    }
+}
