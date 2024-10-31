d46983f4a622302f27496c17b42d19e3cd909c2e
ThomsEADania <thmo5@student.eadania.dk>
2024-10-31 15:11:47 +0100

    Dag 2 - Øvelse 7
    Klared øvelse 7 fra dag 2

---
+++ Dag 2 - Øvelse 7/Animal.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dag_2___Øvelse_7
+{
+    internal abstract class Animal
+    {
+        protected string name;
+        protected int health;
+        protected int legs;
+        protected int age;
+
+        public Animal(string name, int health, int legs, int age)
+        {
+
+        }
+        public virtual void Eat()
+        {
+
+        }
+        public abstract void Sleep();
+    }
+}
